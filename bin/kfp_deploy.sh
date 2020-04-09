3d2f3f22680c039790299c61cfd0c83b73984608
The Combed Thunderclap <clappedthunder@gmail.com>
2020-04-09 11:02:16 +0100

    Debug

--- bin/kfp_deploy.sh
+++ bin/kfp_deploy.sh
@@ -7,16 +7,18 @@ PIPELINE_PATH=${3}
 RUN_ID=$(python3 ${SCRIPT_PATH}/kfp_deploy.py $KUBEFLOW_HOST $EXPERIMENT_NAME $PIPELINE_PATH)
 echo "RUN ID: ${RUN_ID}"
 
-SUCCESS="Succeeded"
-STATUS="Start"
-while [ "${STATUS}" != "${SUCCESS}" ]; do
-  STATUS=$(kfp run list | grep "${RUN_ID}" | awk -F "|" '{ print $4 }')
-  if [ "${STATUS}" == "" ]; then
-    echo "Status: Pending"
-  else
-    echo "Status: ${STATUS}"
-  fi
-  sleep 5
-done
+# SUCCESS="Succeeded"
+# STATUS="Start"
+# while [ "${STATUS}" != "${SUCCESS}" ]; do
+#   STATUS=$(kfp run list | grep "${RUN_ID}" | awk -F "|" '{ print $4 }')
+#   if [ "${STATUS}" == "" ]; then
+#     echo "Status: Pending"
+#   else
+#     echo "Status: ${STATUS}"
+#   fi
+#   sleep 5
+# done
+
+kfp run list
 
 echo "${STATUS}!"

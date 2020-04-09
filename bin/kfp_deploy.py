eab61693568aba456d3f4ff720f7c7a0cfb07e86
The Combed Thunderclap <clappedthunder@gmail.com>
2020-04-09 07:53:25 +0100

    Image should be pushed to analytics project. Python version too old for f-string formatting method.

--- bin/kfp_deploy.py
+++ bin/kfp_deploy.py
@@ -5,11 +5,11 @@ import datetime
 kubeflow_host = sys.argv[1]
 experiment_name = sys.argv[2]
 pipeline_path = sys.argv[3]
-datetime_now = f"{datetime.datetime.now():%Y%m%d_%H%M%S}"
-job_name=f'{experiment_name}_training_{datetime_now}'
+datetime_now = "{0}".format(datetime.datetime.now().strftime('%Y%m%d_%H%M%S'))
+job_name="{0}_training_{1}".format(experiment_name, datetime_now)
 
 client = kfp.Client(host=kubeflow_host)
-exp = client.create_experiment(name=f'{experiment_name}-9')
+exp = client.create_experiment(name="{0}-9".format(experiment_name))
 run = client.run_pipeline(experiment_id=exp.id,job_name=job_name,pipeline_package_path=pipeline_path)
 
 print(run.id)
